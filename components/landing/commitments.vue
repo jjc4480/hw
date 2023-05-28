ed3e439e2aedf91b6b21610084fa6baebde3f27c
장종찬 <jcjang@jangjongchan-ui-MacBookPro.local>
2023-05-28 16:46:17 +0900

    On branch CSS-수정  Your branch is up to date with 'origin/CSS-수�'.
     * 스크롤 바 추가
     * capabilities에 트랜지션 추가
     * capabilities에 아이콘 추가
     * prettier 로 인한 indent 수정
    
     Changes to be committed:
    	modified:   assets/css/landing/capabilities.css
    	modified:   components/landing/capabilities.vue
    	modified:   components/landing/commitments.vue
    	modified:   components/landing/featured.vue
    	modified:   components/landing/infomation.vue
    	modified:   components/layout/footer.vue
    	modified:   pages/index.vue
    	modified:   stores/ui.ts
    
     Changes not staged for commit:
    	modified:   package-lock.json
    	modified:   yarn.lock

--- components/landing/commitments.vue
+++ components/landing/commitments.vue
@@ -1,55 +1,60 @@
 <script setup lang="ts">
-const slideList = [ // 슬라이드 아이템
-  { title: 'ISO 14001', src: 'https://placehold.co/1920x1080' },
-  { title: 'ISO 9001', src: 'https://placehold.co/1440x900' },
-  { title: 'ISO 45001', src: 'https://placehold.co/1280x1024' },
-  { title: 'ISO 14001', src: 'https://placehold.co/1920x1080' },
-  { title: 'ISO 9001', src: 'https://placehold.co/1440x900' },
-  { title: 'ISO 45001', src: 'https://placehold.co/1280x1024' },
-]
+import { Scrollbar } from "swiper";
 
+const slideList = [
+  // 슬라이드 아이템
+  { title: "ISO 14001", src: "https://placehold.co/1920x1080" },
+  { title: "ISO 9001", src: "https://placehold.co/1440x900" },
+  { title: "ISO 45001", src: "https://placehold.co/1280x1024" },
+  { title: "ISO 14001", src: "https://placehold.co/1920x1080" },
+  { title: "ISO 9001", src: "https://placehold.co/1440x900" },
+  { title: "ISO 45001", src: "https://placehold.co/1280x1024" },
+];
 </script>
 
 <template>
   <div>
-    <section
-      class="container mx-auto pt-[20vh]"
-    >
+    <section class="container mx-auto pt-[20vh]">
       <h2
         class="2xl:px-0 px-5 2xl:text-6xl text-4xl text-black font-black leading-snug"
-      >Commitments</h2>
+      >
+        Commitments
+      </h2>
       <p
         class="2xl:px-0 px-5 mt-12 2xl:text-2xl text-lg text-black leading-tight"
-      >Environmental, Quality, Safety 어쩌구 저쩌구~~~ 2줄 정도로 설명~~~
-      대신, 국내 ISO 인증이기에 이 부분이 우려가 된다면, 그냥 Safety, Quality, Environmental 에 대해서 작성. IMPESS 참고. </p>
+      >
+        Environmental, Quality, Safety 어쩌구 저쩌구~~~ 2줄 정도로 설명~~~ 대신,
+        국내 ISO 인증이기에 이 부분이 우려가 된다면, 그냥 Safety, Quality,
+        Environmental 에 대해서 작성. IMPESS 참고.
+      </p>
     </section>
-    <div
-      class="container 2xl:px-0 px-5 mt-48 mx-auto relative"
-    >
-      <fieldset class="flex gap-10 absolute -top-36 right-0">
-        <button
-          class="commitments-swiper-prev group"
-          type="button"
-        >
+    <div class="container 2xl:px-0 px-5 mt-48 mx-auto relative">
+      <!-- <fieldset class="flex gap-10 absolute -top-36 right-0">
+        <button class="commitments-swiper-prev group" type="button">
           <svg
             class="w-8 h-8 fill-black group-disabled:fill-gray-400"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
           >
-            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            <path
+              fill-rule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            />
           </svg>
         </button>
-        <button
-          class="commitments-swiper-next group"
-          type="button"
-        >
+        <button class="commitments-swiper-next group" type="button">
           <svg
             class="w-8 h-8 fill-black group-disabled:fill-gray-400"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
           >
-            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            <path
+              fill-rule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+            />
           </svg>
         </button>
-      </fieldset>
+      </fieldset> -->
       <Swiper
         class="commitments-swiper"
         :setWrapperSize="true"
@@ -79,29 +84,31 @@ const slideList = [ // 슬라이드 아이템
           nextEl: '.commitments-swiper-next',
           prevEl: '.commitments-swiper-prev',
         }"
-        :modules="[SwiperAutoplay, SwiperNavigation]"
+        :modules="[SwiperAutoplay, SwiperNavigation, Scrollbar]"
+        :scrollbar="{
+          draggable: true,
+          el: '.swiper-scrollbar',
+        }"
       >
-        <SwiperSlide
-          v-for="slide in slideList"
-          class="h-auto"
-        >
+        <SwiperSlide v-for="slide in slideList" class="h-auto mb-10">
           <a
             class="block h-full group transition-all duration-500 ease-in-out"
             href="/"
           >
-            <figure
-              class="flex flex-col h-full overflow-hidden"
-            >
+            <figure class="flex flex-col h-full overflow-hidden">
               <img
                 class="object-cover group-hover:transform group-hover:scale-105 transition-all duration-500 ease-in-out"
                 :src="slide.src"
               />
               <figcaption
                 class="flex flex-auto p-3 text-2xl items-end justify-center border-b-2 border-gray-700 group-hover:bg-gray-700 group-hover:text-white transition-all duration-500 ease-in-out"
-              >{{ slide.title }}</figcaption>
+              >
+                {{ slide.title }}
+              </figcaption>
             </figure>
           </a>
         </SwiperSlide>
+        <div class="swiper-scrollbar" />
       </Swiper>
     </div>
   </div>
--- components/landing/infomation.vue
+++ components/landing/infomation.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
+const heading = [
+  // head 애니메이션 요소들 배열 item마다 newline으로 처리
+  "Your Loyal Companion for",
+  "Building a Bright Future",
+];
 
-const heading = [ // head 애니메이션 요소들 배열 item마다 newline으로 처리
-  'Your Loyal Companion for',
-  'Building a Bright Future',
-]
-
-const contents = [ // content 애니메이션 요소들 배열 item마다 newline으로 처리
-  'Built on a foundation of trust, we have experience in delivering engineering design, ',
-  'project management in the semiconductor and display industries.',
-  'We provide unmatched value to our clients, safe work environment to our employees, ',
-  'and excellence to our communities.'
-]
+const contents = [
+  // content 애니메이션 요소들 배열 item마다 newline으로 처리
+  "Built on a foundation of trust, we have experience in delivering engineering design, ",
+  "project management in the semiconductor and display industries.",
+  "We provide unmatched value to our clients, safe work environment to our employees, ",
+  "and excellence to our communities.",
+];
 </script>
 
 <template>
   <div>
     <div class="h-screen relative overflow-hidden">
-      <video class="w-full h-screen object-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" autoplay loop muted>
-        <source src="/media/intro.mp4" type="video/mp4">
+      <video
+        class="w-full h-screen object-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        autoplay
+        loop
+        muted
+      >
+        <source src="/media/intro.mp4" type="video/mp4" />
       </video>
       <div class="container mx-auto pt-80 relative">
-        <div
-          class="pl-8"
-        >
+        <div class="pl-8">
           <section
-            class="grid leading-loose 2xl:px-0 px-5 2xl:text-6xl/[5rem]  text-4xl text-white font-black"
+            class="grid leading-loose 2xl:px-0 px-5 2xl:text-6xl/[5rem] text-4xl text-white font-black"
           >
             <h2
               class="infomation-char flex overflow-hidden"
@@ -35,7 +39,8 @@ const contents = [ // content 애니메이션 요소들 배열 item마다 newlin
                 :class="word === ' ' ? 'mx-2' : ''"
                 v-for="(word, index) in text.split('')"
                 :style="`animation-delay: ${index * 0.05}s`"
-              >{{ word }}</span>
+                >{{ word }}</span
+              >
               <!-- 각각 문자열마다 0.05초 지연하여 애니메이션 -->
             </h2>
           </section>
@@ -51,8 +56,9 @@ const contents = [ // content 애니메이션 요소들 배열 item마다 newlin
                 class="block drop-shadow-lg"
                 :class="word === ' ' ? 'mx-1' : ''"
                 v-for="(word, index) in text.split('')"
-                :style="`animation-delay: ${1 + (index * 0.05)}s`"
-              >{{ word }}</span>
+                :style="`animation-delay: ${1 + index * 0.05}s`"
+                >{{ word }}</span
+              >
               <!-- 각각 문자열마다 0.05초 지연하여 애니메이션 이는 제목보다 느리게 하기위해 +1을 해준다 -->
             </p>
           </article>
