216ea4942ce0e6c7b363b577558c9fd3bff79005
jcjang <jongchanjang90@gmail.com>
2023-06-25 23:10:14 +0900

    On branch 6/25-수정 Your branch is up to date with 'origin/6/25-수�'.
    * footer facebook 링크 추가
    * talent 페이지 추가
    * recruit 페이지 수정
    * 랜딩 페이지 조금 수정
    
    Changes to be committed:
    	modified:   components/landing/commitments.vue
    	modified:   components/layout/footer.vue
    	modified:   composables/nav.ts
    	modified:   pages/capabilities/mechanical.vue
    	modified:   pages/careers/recruit.vue
    	new file:   pages/careers/talent.vue

--- pages/capabilities/mechanical.vue
+++ pages/capabilities/mechanical.vue
@@ -1,4 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Scrollbar } from "swiper"
+
+const slideList = [
+  {
+    title: "LG Display – H1,2 Project",
+    src: "/img/featured_1.jpg",
+    content:
+      "<span>Classification: EPC</span> <span>Location: Haiphong, Vietnam</span>  <span>Construction Period: 2018~2019</span>",
+  },
+  {
+    title: "LG Electronics",
+    src: "/img/featured_4.jpg",
+    content:
+      "<span>Classification: EPC</span> <span>Location: Incheon, South Korea</span> <span>Construction Period: 2017~2023</span>",
+  },
+  {
+    title: "LG Display - P8 & 9 Project",
+    src: "/img/featured_9.jpg",
+    content:
+      "<span>Classification: EPC</span> <span>Location: Paju, South Korea</span> <span>Construction Period: 2016~2022</span>",
+  },
+  {
+    title: "LG Display - P10 Project",
+    src: "/img/featured_10.jpg",
+    content:
+      "<span>Classification: EPC</span> <span>Location: Paju, South Korea</span> <span>Construction Period: 2017~2021</span>",
+  },
+  {
+    title: "LG Display - P8 & 9 Project",
+    src: "/img/featured_9.jpg",
+    content:
+      "<span>Classification: EPC</span> <span>Location: Paju, South Korea</span> <span>Construction Period: 2016~2022</span>",
+  },
+  {
+    title: "LG Display - P10 Project",
+    src: "/img/featured_10.jpg",
+    content:
+      "<span>Classification: EPC</span> <span>Location: Paju, South Korea</span> <span>Construction Period: 2017~2021</span>",
+  },
+]
+</script>
 
 <template>
   <div>
@@ -6,6 +47,79 @@
     <div
       class="capabilities-mechanical py-56 bg-fixed bg-center bg-cover"
     ></div>
-    capabilities/mechanical
+    <section class="container mx-auto pt-[20vh]">
+      <h2
+        class="2xl:px-0 2xl:text-6xl text-4xl text-black font-black leading-snug"
+      >
+        Featured Projects
+      </h2>
+      <p
+        class="2xl:px-0 px-5 mt-12 2xl:text-2xl text-lg text-black leading-tight"
+      >
+        We have a strong connection with clients to this day, and we are
+        maintaining a relationship with clients based on our honest and precise
+        work.
+      </p>
+    </section>
+    <div class="container 2xl:px-0 px-5 mt-2 mx-auto relative">
+      <Swiper
+        class="featured-swiper"
+        :setWrapperSize="true"
+        :slidesPerView="3"
+        :autoplay="{
+          delay: 8000,
+          disableOnInteraction: true,
+        }"
+        :pagination="{
+          clickable: true,
+        }"
+        :breakpoints="{
+          '320': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          '768': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          '1024': {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+        }"
+        :navigation="{
+          nextEl: '.featured-swiper-next',
+          prevEl: '.featured-swiper-prev',
+        }"
+        :modules="[SwiperAutoplay, SwiperNavigation, Scrollbar]"
+        :scrollbar="{
+          draggable: true,
+          el: '.swiper-scrollbar',
+        }"
+      >
+        <SwiperSlide v-for="slide in slideList" class="h-auto mb-10">
+          <a
+            class="block h-full group transition-all duration-500 ease-in-out"
+            href="#"
+          >
+            <figure class="flex flex-col h-full overflow-hidden">
+              <img
+                class="object-fill group-hover:transform group-hover:scale-105 transition-all duration-500 ease-in-out"
+                :src="slide.src"
+              />
+              <figcaption
+                class="flex flex-auto py-8 items-end pl-4 border-gray-700 group-hover:bg-gray-700 group-hover:text-white transition-all duration-500 ease-in-out"
+              >
+                <div class="flex flex-col items-start space-y-8">
+                  <div class="text-xl">{{ slide.title }}</div>
+                  <div class="project_content" v-html="slide.content"></div>
+                </div>
+              </figcaption>
+            </figure>
+          </a>
+        </SwiperSlide>
+        <div class="swiper-scrollbar" />
+      </Swiper>
+    </div>
   </div>
 </template>
---
+++ pages/careers/talent.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+const list = [
+  {
+    icon: "/img/icon-Mechanical.png",
+    title: "창의적인 인재",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  },
+  {
+    icon: "/img/icon-Mechanical.png",
+    title: "창의적인 인재",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  },
+  {
+    icon: "/img/icon-Mechanical.png",
+    title: "창의적인 인재",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  },
+  {
+    icon: "/img/icon-Mechanical.png",
+    title: "창의적인 인재",
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  },
+]
+</script>
+
+<template>
+  <div class="px-8 mx-auto container">
+    <h2 class="text-6xl mt-20 text-center">
+      Talent Lorem ipsum dolor sit amet
+    </h2>
+    <h2 class="font-bold text-6xl my-10 text-center">asdasdff</h2>
+
+    <ul
+      class="flex items-center justify-center flex-wrap border-t-4 border-black pt-20"
+    >
+      <li v-for="item in list" class="flex py-10 border-b">
+        <div class="w-24 mx-12">
+          <img :src="item.icon" />
+        </div>
+
+        <div class="flex flex-col">
+          <div class="flex font-bold text-3xl mb-6">{{ item.title }}</div>
+
+          <div class="flex text-lg">
+            {{ item.content }}
+          </div>
+        </div>
+      </li>
+    </ul>
+
+    <div class="">
+      <a
+        class="lg:flex container justify-center lg:max-w-[100%] lg:h-auto h-20 mx-auto group duration-700 hidden py-20 delay-75"
+        href="/careers/recruit"
+      >
+        <section
+          class="lg:w-[55%] flex flex-col justify-center w-full lg:h-auto h-full lg:pl-16 pl-0 lg:bg-hw bg-black/50 z-10 text-center"
+        >
+          <h2
+            class="2xl:text-5xl text-3xl text-white font-black leading-snug lg:text-left text-center"
+          >
+            Careers
+          </h2>
+          <p
+            class="mt-10 2xl:text-xl text-lg text-white leading-tight lg:text-left text-center"
+          >
+            We embrace people of all backgrounds, experiences,<br />
+            culture, styles and talents. Join us!
+          </p>
+          <div class="lg:flex hidden mt-8">
+            <span class="text-white 2xl:text-2xl lg:text-lg">View More</span>
+            <svg
+              class="w-8 h-8 ml-10 group-hover:ml-16 fill-white duration-150"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </div>
+        </section>
+        <figure class="lg:w-[25%] w-full overflow-hidden">
+          <img
+            class="w-full h-full group-hover:scale-110 duration-500 object-cover object-center"
+            src="/img/helena-hertz-wWZzXlDpMog-unsplash.png"
+          />
+        </figure>
+      </a>
+    </div>
+  </div>
+</template>
