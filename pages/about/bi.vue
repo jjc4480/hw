cf027242088fd2cb43fba6b1b74a5185727f0413
jjc4480 <jongchanjang90@gmail.com>
2023-07-10 16:58:29 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    * bi 반응형 추가
    
    On branch 7/10-수정
    Your branch is up to date with 'origin/7/10-수정'.
    
    Changes to be committed:
    	modified:   pages/about/bi.vue

--- pages/about/bi.vue
+++ pages/about/bi.vue
@@ -23,12 +23,12 @@ const infomation = ref() // 소개영역
       <h2 class="text-6xl mb-20 text-left text-hw font-extrabold">
         Brand Identity
       </h2>
-      <figure class="flex gap-20 pb-16">
+      <figure class="flex flex-col xl:flex-row gap-20 pb-16">
         <div class="flex w-full flex-col h-fit my-auto space-y-20 items-center">
           <img class="w-3/5 flex" src="/img/logo_header_large_b.png" />
           <img class="w-3/5 flex" src="/img/logo_header_b.png" />
         </div>
-        <figcaption class="border-l-2 pl-20 border-black">
+        <figcaption class="xl:border-l-2 xl:pl-20 xl:border-black">
           <h3 class="text-4xl font-bold">
             This is HYOUNGWON BI with a firm philosophy and modern sensibility.
           </h3>
@@ -44,48 +44,48 @@ const infomation = ref() // 소개영역
         </figcaption>
       </figure>
 
-      <div class="grid grid-cols-5 pt-16 border-t-2 border-black">
-        <div class="flex items-center justify-center space-x-4">
+      <div class="grid grid-cols-2 xl:grid-cols-5 pt-16 border-t-2 border-black gap-5 ">
+        <div class="flex items-center xl:justify-center justify-stretch w-full lg:w-1/2 xl:w-full mx-auto  ">
           <div
             class="rounded-[50%] border-[#009CA6] w-20 border-[6px] h-20"
           ></div>
-          <div class="flex flex-col">
+          <div class="flex flex-col ml-4 text-left">
             <span class="flex">HW Light Sea Green</span>
             <span class="flex">pantone 320C</span>
           </div>
         </div>
-        <div class="flex items-center justify-center space-x-4">
+        <div class="flex items-center xl:justify-center justify-stretch w-full lg:w-1/2 xl:w-full mx-auto  ">
           <div
             class="rounded-[50%] border-[#008C95] w-20 border-[6px] h-20"
           ></div>
-          <div class="flex flex-col">
+          <div class="flex flex-col ml-4 text-left">
             <span class="flex">HW Teal</span>
             <span class="flex">pantone 321C</span>
           </div>
         </div>
-        <div class="flex items-center justify-center space-x-4">
+        <div class="flex items-center xl:justify-center justify-stretch w-full lg:w-1/2 xl:w-full mx-auto  ">
           <div
             class="rounded-[50%] border-[#10446D] w-20 border-[6px] h-20"
           ></div>
-          <div class="flex flex-col">
+          <div class="flex flex-col ml-4 text-left">
             <span class="flex">HW Midnight Blue</span>
             <span class="flex">pantone 7694C</span>
           </div>
         </div>
-        <div class="flex items-center justify-center space-x-4">
+        <div class="flex items-center xl:justify-center justify-stretch w-full lg:w-1/2 xl:w-full mx-auto  ">
           <div
             class="rounded-[50%] border-[#2D2926] w-20 border-[6px] h-20"
           ></div>
-          <div class="flex flex-col">
+          <div class="flex flex-col ml-4 text-left">
             <span class="flex">HW Black</span>
             <span class="flex">pantone Black C</span>
           </div>
         </div>
-        <div class="flex items-center justify-center space-x-4">
+        <div class="flex items-center xl:justify-center justify-stretch w-full lg:w-1/2 xl:w-full mx-auto  ">
           <div
             class="rounded-[50%] border-[#3F4444] w-20 border-[6px] h-20"
           ></div>
-          <div class="flex flex-col">
+          <div class="flex flex-col ml-4 text-left">
             <span class="flex">HW Dark Gray</span>
             <span class="flex">pantone 446C</span>
           </div>
